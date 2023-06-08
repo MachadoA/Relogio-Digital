1a6a958754235c051fe6c6b92818daca50f880b4
devMachadoA <amachadoamaral2@gmail.com>
2023-06-08 21:59:08 +0100

    Primeiro commit - sem responsividade

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [hours, setHours] = useState("00");
+  const [minutes, setMinutes] = useState("00");
+  const [seconds, setSeconds] = useState("00");
+
+  useEffect(() => {
+    const intervalID = setInterval(() => updateTime(), 1000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
+  const formatTime = (time) => {
+    return time.toString().padStart(2, "0");
+  };
+
+  const updateTime = () => {
+    const now = new Date();
+    const hours = formatTime(now.getHours());
+    const minutes = formatTime(now.getMinutes());
+    const seconds = formatTime(now.getSeconds());
+    setHours(hours);
+    setMinutes(minutes);
+    setSeconds(seconds);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="clock">
+        <span className="hours">{hours}</span>
+        <span className="divider">:</span>
+        <span className="minutes">{minutes}</span>
+        <span className="divider">:</span>
+        <span className="seconds">{seconds}</span>
+      </div>
     </div>
   );
 }
